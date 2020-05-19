150ad2651dce1a914d1066d4ba33f2575eae51d3
Patrick Henninger <patrick.henninger@contagt.com>
2020-05-19 22:37:42 +0200

    Add placeholder tests to suppress warnings, Test ServerConnectionInfo timeout validation,

---
+++ tests/Unit/ServerConnectionInfoTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace DSG\SquadRCON\Tests\Unit;
+
+use DSG\SquadRCON\Data\ServerConnectionInfo;
+
+class ServerConnectionInfoTest extends \DSG\SquadRCON\Tests\TestCase {
+    /**
+     * Validates the ServerConnectionInfo can be initialized
+     * 
+     * @return void
+     */
+    public function test_server_connection_info_can_be_initialized()
+    {
+        $info = new ServerConnectionInfo('localhost', 12345, 'secret');
+        
+        $this->assertIsObject($info);
+    }
+
+    /**
+     * Validates that the provided timeout is validated.
+     * 
+     * @return void
+     */
+    public function test_server_connection_info_timeout_being_validated()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        
+        new ServerConnectionInfo('localhost', 12345, 'secret', 0);
+    }
+}
--- tests/Unit/SquadTest.php
+++ tests/Unit/SquadTest.php
@@ -3,5 +3,13 @@
 namespace DSG\SquadRCON\Tests\Unit;
 
 class SquadTest extends \DSG\SquadRCON\Tests\TestCase {
-    //
+    /**
+     * Placeholder
+     * 
+     * @return void
+     */
+    public function test_placeholder()
+    {
+        $this->assertTrue(true);
+    }
 }
